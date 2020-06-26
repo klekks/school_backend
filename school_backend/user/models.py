75e3df200a90b9d0eec5c695389947d37e3bc1e6
Ilya Petrov <klekks@yandex.ru>
2020-06-27 02:17:52 +0500

    0.0.2

--- school_backend/user/models.py
+++ school_backend/user/models.py
@@ -1,4 +1,6 @@
 from django.db import models
 
 class User(models.Model):
-    name = models.CharField(max_length)
+    id = models.BigIntegerField(primary_key=True, unique=True)
+    name = models.CharField(max_length=64)
+    surname = models.CharField(max_length=64)
